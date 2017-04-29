64b6ce243eaaa8acd61309806a0ff0da9d64d51a
TJ Draper <tj@buzzingpixel.com>
2017-04-29 15:00:27 -0700

    Add copyright and licensing info banner to files

--- src/grunt/javascriptConfig.js
+++ src/grunt/javascriptConfig.js
@@ -1,3 +1,11 @@
+/*------------------------------------------------------------------------------
+Copyright 2017, BuzzingPixel, LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Apache License 2.0.
+http://www.apache.org/licenses/LICENSE-2.0
+------------------------------------------------------------------------------*/
+
 /* globals module, __dirname */
 
 module.exports = function(grunt) {
