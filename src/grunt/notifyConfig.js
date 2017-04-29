a4c4df36bdc7475beced22d0cc8203b7fed92c86
TJ Draper <tj@buzzingpixel.com>
2017-04-29 15:13:11 -0700

    Refactor banner comment headings

--- src/grunt/notifyConfig.js
+++ src/grunt/notifyConfig.js
@@ -1,10 +1,10 @@
-/*------------------------------------------------------------------------------
-Copyright 2017, BuzzingPixel, LLC
+/*----------------------------------------------------------------------------*\
+    # Copyright 2017, BuzzingPixel, LLC
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the Apache License 2.0.
-http://www.apache.org/licenses/LICENSE-2.0
-------------------------------------------------------------------------------*/
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the Apache License 2.0.
+    # http://www.apache.org/licenses/LICENSE-2.0
+\*----------------------------------------------------------------------------*/
 
 /* globals module */
 
